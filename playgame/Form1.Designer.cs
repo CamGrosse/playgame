06e461246814560b8e32761a2cb06f849dc8e4c4
camegros192 <camegros192@SNS-D-130S04>
2022-02-09 14:03:34 -0500

    Add project files.

---
+++ playgame/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace playgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Font = new System.Drawing.Font("Ravie", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(12, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PLay Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(170, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 92);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Game starts in...";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(429, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 92);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PlayGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ playgame/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace playgame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Visible=false;
+            label4.Visible=false;   
+
+        }
+
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            button1.Visible = false;
+            
+            Refresh();
+            Thread.Sleep(500);
+            
+            label3.Visible=true;
+            label4.Visible=true;
+            
+            Refresh();
+            Thread.Sleep(2000);
+           
+            label4.Text="2";
+           
+            Refresh();
+            Thread.Sleep(2000);
+           
+            label4.Text ="1";
+            Refresh();
+           
+            Thread.Sleep(2000);
+            
+            label4.Visible=false;
+            label3.Visible=false;
+           
+            Refresh();
+            Thread.Sleep(200);
+           
+            this.BackColor=Color.Green;
+
+        }
+
+        
+    }
+}
